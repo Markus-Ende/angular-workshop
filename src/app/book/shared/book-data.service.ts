32bae9c696f8a21968c190f05261aaa5d16dcada
web-dave <dave@webdave.de>
2018-05-08 11:14:45 +0200

    update angular to v6
    npm i @angular/cli@6.0.0
    ng update @Angular/cli
    npm install -g rxjs-tslint
    rxjs-5-to-6-migrate -p src/tsconfig.app.json

--- src/app/book/shared/book-data.service.ts
+++ src/app/book/shared/book-data.service.ts
@@ -1,28 +1,30 @@
-import { Injectable } from '@angular/core';
-import { Book } from './book';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+
+import {of as observableOf,  Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Book } from "./book";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class BookDataService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {
+  getBooksArray(): Observable<Book[]> {
+    return observableOf([]);
   }
 
   getBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>('http://localhost:4730/books')
+    return this.http.get<Book[]>("http://localhost:4730/books");
   }
 
   getBookByIsbn(isbn: string): Observable<Book> {
-    return this.http.get<Book>(`http://localhost:4730/books/${isbn}`)
+    return this.http.get<Book>(`http://localhost:4730/books/${isbn}`);
   }
 
   createBook(book: Book): Observable<Book> {
-    return this.http.post<Book>('http://localhost:4730/books', book)
+    return this.http.post<Book>("http://localhost:4730/books", book);
   }
 
   updateBook(isbn: string, vector: any): Observable<Book> {
-    return this.http.patch<Book>(`http://localhost:4730/books/${isbn}`, vector)
+    return this.http.patch<Book>(`http://localhost:4730/books/${isbn}`, vector);
   }
-
 }
